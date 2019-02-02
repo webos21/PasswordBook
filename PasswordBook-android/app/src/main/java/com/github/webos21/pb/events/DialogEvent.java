9369b68a498aa8aa7ee30f98097261a4bb11253b
Cheolmin Jo <webos21@gmail.com>
2019-02-03 01:29:43 +0900

    * PasswordBook : android  - back to my code

--- PasswordBook-android/app/src/main/java/com/github/webos21/pb/events/DialogEvent.java
+++
@@ -1,7 +0,0 @@
-package com.github.webos21.pb.events;
-
-/**
- * Created by bob.sun on 16/5/21.
- */
-public class DialogEvent {
-}
---
+++ PasswordBook-android/app/src/main/java/com/gmail/webos21/passwordbook/keypad/KeypadButtonCategory.java
@@ -0,0 +1,7 @@
+package com.gmail.webos21.passwordbook.keypad;
+
+public enum KeypadButtonCategory {
+	NUMBER
+	, OPERATOR
+	, DUMMY
+}
